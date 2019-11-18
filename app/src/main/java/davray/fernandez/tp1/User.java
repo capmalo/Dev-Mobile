9289e665f861c7e9f57c963fe14e71740f11be02
Theodav <theodore.d.avray@efrei.net>
2019-11-18 17:14:19 +0100

    Object user with all needed informations

--- app/src/main/java/davray/fernandez/tp1/User.java
+++ app/src/main/java/davray/fernandez/tp1/User.java
@@ -4,24 +4,41 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class User {
-    //@SerializedName("name")
-    //private String name;
     @SerializedName("email")
     @Expose
     private String email;
 
-    public User(String email) {
-        //this.name = name;
+    @SerializedName("name")
+    @Expose
+    private Object name;
+
+    @SerializedName("login")
+    @Expose
+    private Object login;
+
+    @SerializedName("dob")
+    @Expose
+    private Object dob;
+
+    @SerializedName("picture")
+    @Expose
+    private Object picture;
+
+    public User(String email, Object name, Object login, Object dob, Object picture) {
+        this.name = name;
         this.email = email;
+        this.login = login;
+        this.dob = dob;
+        this.picture = picture;
     }
 
-    /*public String getName() {
+    public Object getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(Object name) {
         this.name = name;
-    }*/
+    }
 
     public String getEmail() {
         return email;
@@ -29,6 +46,30 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Object getLogin() {
+        return login;
+    }
+
+    public void setLogin(Object login) {
+        this.login = login;
+    }
+
+    public Object getDob() {
+        return dob;
+    }
+
+    public void setDob(Object dob) {
+        this.dob = dob;
+    }
+
+    public Object getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Object picture) {
+        this.picture = picture;
     }
 }
 
